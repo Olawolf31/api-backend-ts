2a5538c42b8089029c1732d82e1c1f62e6a700ab
Olayiwola Onatunde <Olayiwola@MacBook-Air-von-admin.local>
2023-04-20 13:12:16 +0200

    completed CRUD

--- src/controllers/moviesLogic.ts
+++ src/controllers/moviesLogic.ts
@@ -1,8 +1,7 @@
 import { RequestHandler, Request, Response } from "express";
 import { Movies } from "../models/movies";
 
-//Movies routes
-
+//movies routes
 export const addNewMovie: RequestHandler = async (
   req: Request,
   res: Response
@@ -34,6 +33,72 @@ export const getAllMovies: RequestHandler = async (
     res.status(200).json({
       message: "returned all movies",
       movies: movies,
+    });
+  } catch (error: unknown) {
+    if (typeof error === "string") {
+      res.status(400).json({ message: error });
+    } else if (error instanceof Error) {
+      res.status(400).json({ message: error.message });
+    }
+  }
+};
+
+//get movie by id
+export const getSingleMovie: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const { id } = req.params;
+    const movie = await Movies.findById(id);
+    res.status(200).json({
+      message: "returned single movie",
+      movie: movie,
+    });
+  } catch (error: unknown) {
+    if (typeof error === "string") {
+      res.status(400).json({ message: error });
+    } else if (error instanceof Error) {
+      res.status(400).json({ message: error.message });
+    }
+  }
+};
+
+//delete movie by id
+export const deleteMovie: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const { id } = req.params;
+    await Movies.findByIdAndDelete(id);
+    res.status(200).json({
+      message: "movie deleted",
+    });
+  } catch (error: unknown) {
+    if (typeof error === "string") {
+      res.status(400).json({ message: error });
+    } else if (error instanceof Error) {
+      res.status(400).json({ message: error.message });
+    }
+  }
+};
+
+//update movie by id
+export const updateMovie: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const updatedMovie = await Movies.findByIdAndUpdate(
+      (req.params.id, req.body),
+      {
+        new: true,
+      }
+    );
+    res.status(200).json({
+      message: "movie updated",
+      updatedMovie,
     });
   } catch (error: unknown) {
     if (typeof error === "string") {
--- src/routes/moviesRoutes.ts
+++ src/routes/moviesRoutes.ts
@@ -1,8 +1,18 @@
 import { Router } from "express";
-import { addNewMovie, getAllMovies } from "../controllers/moviesLogic";
+import {
+  addNewMovie,
+  getAllMovies,
+  getSingleMovie,
+  deleteMovie,
+  updateMovie,
+} from "../controllers/moviesLogic";
 
 const router = Router();
 
-router.post("/", addNewMovie)
-router.get("/", getAllMovies)
+router.post("/", addNewMovie);
+router.get("/", getAllMovies);
+router.get("/:id", getSingleMovie);
+router.delete("/:id", deleteMovie);
+router.put("/:id", updateMovie);
+
 export default router;
